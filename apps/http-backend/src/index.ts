c553f28e935062e2060f95fe4b8164521f413143
Shuvkant <weird426344@gmail.com>
2026-07-03 23:00:38 +0545

    prisma database

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,35 +1,61 @@
 import express from "express"
 import jwt from "jsonwebtoken"
-import { middleware } from "./middleware"
+import { middleware } from "./middleware.js"
 import { JWT_SECRET } from "@repo/backend-common/config"
 import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types"
+import { prisma } from "@repo/db"
 
 const app = express()
 app.use(express.json())
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  console.log(prisma)
+  //   const data = await prisma.user.findFirst()
+  //   res.status(200).json({
+  //     msg: data?.email
+  //   })
+})
+app.post("/signup", async (req, res) => {
+
   const parsedSignup = CreateUserSchema.safeParse(req.body)
   if (!parsedSignup.success) {
     return res.status(411).json({
-      msf: "Invalid format "
+      msg: "Invalid format "
     })
   }
-  res.status(200).json({
-    msg: "You are welcome"
-  })
-})
-app.post("/signup", (req, res) => {
-  res.status(200).json({
-    msg: "Successfully signed up"
-  })
+  try {
 
+    const data = await prisma.user.create({
+      data: {
+        name: parsedSignup.data?.name,
+        email: parsedSignup.data?.email,
+        password: parsedSignup.data?.password,
+        photo: parsedSignup.data?.photo
+      }
+    })
+    res.status(200).json({
+      msg: "Signup successful",
+      id: data.id
+    })
 
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(error.message);
+
+      return res.status(500).json({
+        msg: error.message
+      })
+    } else {
+      console.log("An unexpected error occurred:", error);
+    }
+
+  }
 })
 
 app.post("/signin", (req, res) => {
   const parsedSignin = SigninSchema.safeParse(req.body)
   if (!parsedSignin.success) {
     return res.status(411).json({
-      msf: "Invalid format "
+      msg: "Invalid format "
     })
   }
   const userId = 12
@@ -47,10 +73,10 @@ app.post("/room", middleware, (req, res) => {
   const parsedRoom = CreateRoomSchema.safeParse(req.body)
   if (!parsedRoom.success) {
     return res.status(411).json({
-      msf: "Invalid format "
+      msg: "Invalid format "
     })
   }
 })
 app.listen(3000, () => {
-  console.log("Server started ")
+  console.log("Server started  at port 3000")
 })
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -2,7 +2,8 @@ import * as z from "zod"
 export const CreateUserSchema = z.object({
   email: z.email(),
   password: z.string().min(5).max(50),
-  userName: z.string().min(3).max(50)
+  name: z.string().min(3).max(50),
+  photo: z.string().optional()
 
 })
 
