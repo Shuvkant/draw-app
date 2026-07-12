5253396e0a1dcf0ac3d0b186386dd182f26b0304
Shuvkant <weird426344@gmail.com>
2026-07-12 23:13:19 +0545

    restructure canvas rect draw

--- apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,22 +1,20 @@
-"use client"
+"use client";
 
+import { InitDraw } from "@/app/draw";
 import { useEffect, useRef } from "react"
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-      if (!ctx) {
-        return
-      }
 
-      ctx.strokeRect(25, 25, 100, 100)
+  useEffect(() => {
+
+    if (canvasRef.current) {
+      InitDraw(canvasRef.current)
     }
 
-  }, [canvasRef])
+  }, [canvasRef]);
+
   return <div>
-    <canvas ref={canvasRef} width={500} height={500}></canvas>
+    <canvas ref={canvasRef} width={2000} height={1000}></canvas>
   </div>
 }
